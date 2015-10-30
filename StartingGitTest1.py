ea8cdb3c9489dda0c31b1f7d80bc19809b3a7851
luludoobee <bcdean03@gmail.com>
2015-10-30 16:16:58 -0600

    Trying to merge

--- StartingGitTest1.py
+++ StartingGitTest1.py
@@ -2,7 +2,6 @@ __author__ = 'Dean, Abel'
 print("Abel Amadou")
 print("goober")
 s = input("hello abel ")
-kfjasdf ksj
-print "This is not going to wwork"
+print("This is not going to wwork")
 s = input("hello abel")
 print(s)
