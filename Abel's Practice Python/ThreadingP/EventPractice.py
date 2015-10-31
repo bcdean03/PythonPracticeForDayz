bfeb44425d15415fbb7eec78c8ce2e9cf990e7f6
abelamadou <abelamadou@msudenver.edu>
2015-10-31 01:20:28 -0600

    Made some changes(Just playing with events) to EventPractice Class in [SmokerProcucer]

--- Abel's Practice Python/ThreadingP/EventPractice.py
+++ Abel's Practice Python/ThreadingP/EventPractice.py
@@ -6,25 +6,27 @@ event = Event()
 
 class Producer(Thread):
     def run(self):
-        while True:
-            print "Going to set it"
+        for _ in xrange(2):
+            print "Producer Going to set it"
             # time.sleep(1)
             event.set()
             event.clear()
-            print "Cleared it"
-            time.sleep(5)
+            print "Producer Cleared it"
+            time.sleep(3)
             # event.set()
+        print("!!!Producer TERMINATING!!!")
 
 class Smoker(Thread):
     def run(self):
         while True:
-            print "Waiting for Produce to notify me "
+            print "{} Waiting for Producer to notify me".format(self.name)
             event.wait()
-            print "Done waiting"
+            print "{} Done waiting".format(self.name)
             time.sleep(2)
 
 def main():
-    s = Smoker().start()
+    s1 = Smoker(name="Smk1").start()
+    s2 = Smoker(name="Smk2").start()
     p = Producer().start()
 
 
