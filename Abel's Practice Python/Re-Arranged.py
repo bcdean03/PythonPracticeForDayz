2ad51b338d673435f182003a74d38c09b80eb57a
abelamadou <abelamadou@msudenver.edu>
2015-11-03 11:50:20 -0700

    sorting

---
+++ Abel's Practice Python/Re-Arranged.py
@@ -0,0 +1,13 @@
+__author__ = 'abelamadou'
+# from statistics import * #Works with 3.5
+# https://docs.python.org/2/library/functions.html#sorted
+a =[5.9,7.2,7.6,6.8,8.2,8.7,7.8,7.7,7.3,6.3,6.5,7.0,7.8,9.7,11.6,11.3,8.1,6.8,7.0,6.3,7.9,9.0,7.4,7.7,9.7,11.8,10.7]
+print(a)
+a.sort()
+print(a)
+print (len(a))
+b = sorted(a)
+print('Sorted:', b)
+# print( 'Median:',median(b)) #Works with 3.5
+
+
